f0c8df38f5a73ae45710ca7d11e7b2a848daa7bb
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-13 15:27:38 -0300

    exercicio 04

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,10 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String texto1 = "Holberton";
+        String texto2 = "School";
+
+        System.out.printf("Bem vindo a "+texto1+" "+texto2+"!\n");
+
+
+    }
+}
--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,6 +1,7 @@
 public class PrintStringVariable {
     public static void main(String[] args) {
         String texto = "Holberton School";
-        System.out.printf(texto +"\n" +texto+"\n"+texto.substring(0,10));
+
+        System.out.printf(texto +"\n"+texto+"\n"+texto.substring(0,8)+"\n");
     }
 }
