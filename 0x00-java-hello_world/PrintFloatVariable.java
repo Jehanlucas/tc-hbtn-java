f2d8b4450d9c441d98a0c40655a7389a0d07bd66
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-13 14:06:55 -0300

    exercicio 01

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String[] args) {
-        float Pi = 3.1425f;
+        float Pi = 3.14212f;
         System.out.printf("%.3f Valor de pi:", Pi);
     }
 }
