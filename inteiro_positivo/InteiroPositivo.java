490f168741686828a51b42b24de6b65616f54dec
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-14 15:35:49 -0300

    Tipos primitivos

---
+++ inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,33 @@
+public class InteiroPositivo {
+    public int valor ;
+
+    public InteiroPositivo(int valor) {
+        this.valor = Integer.valueOf(valor);
+    }
+
+    public InteiroPositivo(String valor) {
+        this.valor = Integer.parseInt(valor);
+    }
+
+    public  boolean ehPrimo(){
+        for (int i = 2; i < valor; i++) {
+            if (valor % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public int getValor()  throws IllegalArgumentException {
+        if(valor < 0 )
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+
+        return valor ;
+    }
+
+    public void setValor(int valor) throws IllegalArgumentException {
+        if(valor < 0 )
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        this.valor = valor ;
+    }
+
+}
