addd7c9122df2046d5fd883580019c65ff7cf9f5
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-15 12:34:50 -0300

    exercicio 02 Classes e Objetos

--- 0x04/metodos/PersonagemGame.java
+++ 0x04/metodos/PersonagemGame.java
@@ -1,10 +1,6 @@
 public class PersonagemGame {
-
     private int saudeAtual ;
-
     private String nome ;
-
-
     public int tomarDano(int quantidadeDeDano){
         if(quantidadeDeDano >= saudeAtual){
             return  this.saudeAtual = 0;
@@ -12,38 +8,33 @@ public class PersonagemGame {
         if (saudeAtual >0) {
             return saudeAtual -= quantidadeDeDano;
         }
-
         return quantidadeDeDano;
     }
-
     public int receberCura(int quantidadeDeCura){
-        if(quantidadeDeCura >= 100){
-            return  this.saudeAtual = saudeAtual-1;
-        }
-        if (saudeAtual >0) {
-            return saudeAtual += quantidadeDeCura-1;
-        }
 
+        if (saudeAtual <=100 && quantidadeDeCura < 100) {
+            if (saudeAtual > 0 && saudeAtual <= 100) {
+                saudeAtual += quantidadeDeCura-1;
+                return quantidadeDeCura;
+            }
+        }
+        else{
+            return saudeAtual = 0;
+        }
         return quantidadeDeCura;
     }
-
-
 
 
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public int getSaudeAtual() {
         return saudeAtual;
     }
-
     public void setSaudeAtual(int saudeAtual) {
         this.saudeAtual = saudeAtual;
     }
 }
-
