3ee08f725144d301cf166510db6dcc7bef62b450
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-21 11:44:18 -0300

    numeros de 0 a 99 pares

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,14 @@
+
+public class NumerosPares {
+    public static void main(String[] args) {
+
+        for (int n = 0; n <= 97; n++) {
+            if (n % 2 == 0) {
+                System.out.print(n+", ");
+
+            }
+
+        }
+        System.out.print(98+"\n");
+    }
+}
