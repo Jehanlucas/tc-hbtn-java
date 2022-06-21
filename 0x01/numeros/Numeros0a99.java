f7b00cb3583e9610563d3d3f6d222d4c38ebfa0f
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-21 11:41:11 -0300

    numeros de 0 a 99

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -3,7 +3,7 @@ public class Numeros0a99 {
     public static void main(String[] args) {
 
         for (int n = 0; n <= 98; n++) {
-                System.out.print(n+" ,");
+                System.out.print(n+", ");
 
 
 
