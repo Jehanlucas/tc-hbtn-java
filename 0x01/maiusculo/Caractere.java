bc4a37654be9cb2a85667dc49223be9d1edc723b
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-21 14:40:06 -0300

    Letra Maiuscula

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,18 @@
+public class Caractere {
+
+
+    public static boolean EhMaiusculo(char c){
+        if(c >65 && c <= 91){
+            return true ;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
+
+
+
+}
