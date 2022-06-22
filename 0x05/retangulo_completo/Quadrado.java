27097762d8b461ce201620c1e16a84a1feb7b66a
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-22 14:22:35 -0300

    Herança Task 3

---
+++ 0x05/retangulo_completo/Quadrado.java
@@ -0,0 +1,16 @@
+public class Quadrado {
+
+
+    private static double area = 0 ;
+    public static double area(double lado){{
+
+            if (lado > 0){area = lado *lado ;}
+
+            else{
+
+                throw new IllegalArgumentException("Lado deve possuir valor positivo");
+            }
+
+}
+        return area;
+    }}
