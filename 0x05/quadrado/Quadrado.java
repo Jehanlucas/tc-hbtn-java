9f24fad47161a229a16f8cf331bd7a353175c594
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-22 14:54:28 -0300

    Herança Task 4

---
+++ 0x05/quadrado/Quadrado.java
@@ -0,0 +1,47 @@
+public class Quadrado extends Retangulo {
+
+
+    private static double lado;
+
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+
+        if (lado >= 0){
+            this.lado = lado;
+        }
+        else {
+            throw new java.lang.IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        }
+    }
+
+    public static double getArea() {
+        return area;
+    }
+
+    public static void setArea(double area) {
+        Quadrado.area = area;
+    }
+
+    private static double area = 0 ;
+    public double area(){{
+
+            if (lado > 0){area = lado *lado ;}
+
+            else{
+
+                throw new IllegalArgumentException("Lado deve possuir valor positivo");
+            }
+
+}
+        return area;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Quadrado] " + lado);
+    }
+}
