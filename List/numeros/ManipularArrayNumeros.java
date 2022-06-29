e4dc52a10b6a8fc0c8f075c4b711529c01049f55
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-28 22:34:47 -0300

    List  task 0

---
+++ List/numeros/ManipularArrayNumeros.java
@@ -0,0 +1,35 @@
+
+import java.util.List;
+
+public class ManipularArrayNumeros {
+
+    public static int buscarPosicaoNumero(List<Integer> lista, int numero){
+        return  lista.indexOf(numero);
+    }
+
+    public static void adicionarNumero(List<Integer> lista, int numero) throws IllegalArgumentException{
+        int aux = buscarPosicaoNumero(lista, numero);
+        if(aux < 0){
+            lista.add(numero);
+        }else
+            throw new IllegalArgumentException("Numero jah contido na lista");
+    }
+
+    public static void removerNumero(List<Integer> lista, int numero) throws IllegalArgumentException {
+        int i = buscarPosicaoNumero(lista, numero);
+        if(i != -1){
+            lista.remove(Integer.valueOf(numero));
+        }else
+            throw new IllegalArgumentException("Numero nao encontrado na lista");
+    }
+
+    public static void substituirNumero(List<Integer> lista, int numeroSubstituir, int numeroSubstituto){
+        int i = buscarPosicaoNumero(lista, numeroSubstituir);
+        if(i < 0){
+            lista.add(numeroSubstituto);
+        }else{
+            lista.set(i, numeroSubstituto);
+        }
+    }
+
+}
