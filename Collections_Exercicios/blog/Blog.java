ceeb5039dcefe50f71b79d41d296a20bd2eeed37
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-06-30 11:29:12 -0300

    Set e Map Task Collections

--- Collections_Exercicios/blog/Blog.java
+++ Collections_Exercicios/blog/Blog.java
@@ -1,24 +1,23 @@
-
 import java.util.*;
 
 public class Blog {
-      List<Post> postagem = new ArrayList<>();
+      List<Post> postagens = new ArrayList<>();
      String categoria;
 
 
     public Blog() {
-        postagem = new ArrayList<>();
+        postagens = new ArrayList<>();
     }
 
 
     public  void adicionarPostagem(Post post) {
-        postagem.add(post);
+        postagens.add(post);
     }
 
     public  Set<String> obterTodosAutores() {
         Set<String> autor = new TreeSet<>();
 
-        for (Post post : postagem) {
+        for (Post post : postagens) {
 
             autor.add(post.getAutor());
 
@@ -33,13 +32,13 @@ public class Blog {
 
         int total=0;
 
-        for(int i = 0; i < postagem.size(); i++) {
-            for(int j = 0; j < postagem.size() ; j++) {
-                if(postagem.get(i).getCategoria().equals(postagem.get(j).getCategoria())) {
+        for(int i = 0; i < postagens.size(); i++) {
+            for(int j = 0; j < postagens.size() ; j++) {
+                if(postagens.get(i).getCategoria().equals(postagens.get(j).getCategoria())) {
                     total++;
                 }
             }
-            contador.put(postagem.get(i).getCategoria(), total);
+            contador.put(postagens.get(i).getCategoria(), total);
             total = 0;
         }
         return contador;
