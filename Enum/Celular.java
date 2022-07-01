03d3fd49a6aefc82e790aa44e11bf30b2acec02b
Jehan Lucas Vieira e Silva <CIANDT\jehan@lnb035965cps.ciandt.global>
2022-07-01 16:05:56 -0300

    ENUM Task  0

---
+++ Enum/Celular.java
@@ -0,0 +1,60 @@
+
+import java.util.ArrayList;
+
+public class Celular {
+
+    static ArrayList<Contato> telefone = new ArrayList<>();
+
+    public static int obterPosicaoContato(String nomeContato){
+        for (Contato nome: telefone ) {
+            if (nome.getNome().equals(nomeContato)){
+                return telefone.indexOf(nome) ;
+            }
+        }
+        return -1;
+    }
+
+
+
+    public static void adicionarContato(Contato contato) {
+        int aux = obterPosicaoContato(contato.getNome());
+        if(aux != -1){
+            throw new IllegalArgumentException("Nao foi possivel adicionar contato. Contato jah existente com esse nome");
+        }else {
+            telefone.add(contato);
+        }
+    }
+
+
+
+    public static void atualizarContato(Contato contatoAntigo, Contato novoContato){
+        int i = obterPosicaoContato(contatoAntigo.getNome());
+        int j = obterPosicaoContato(novoContato.getNome());
+
+
+         if (i < 0 ){
+            throw new IllegalArgumentException("Nao foi possivel modificar contato. Contato nao existe");
+        }
+        if (j!= -1 && j != i){
+            throw new IllegalArgumentException("Nao foi possivel modificar contato. Contato jah existente com esse nome");
+        }
+
+         telefone.set(i,novoContato);
+    }
+
+    public static void removerContato(Contato contato) {
+        int i = obterPosicaoContato(contato.getNome());
+        if(i != -1){
+            telefone.remove(i);
+        }else
+            throw new IllegalArgumentException("Nao foi possivel remover contato. Contato nao existe");
+    }
+
+    public void listarContatos(){
+        for (Contato lista : telefone){
+             System.out.println(lista.getNome()+" -> "+ lista.getNumero() +" ("+lista.getTipo()+")");
+        }
+    }
+
+
+}
