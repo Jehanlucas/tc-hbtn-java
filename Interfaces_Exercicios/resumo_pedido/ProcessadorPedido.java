8f19d80a95e0c0eaf14763e8ffb1e129aaa5d80a
Jehanlucas <jehan@ciandt.com>
2022-07-05 14:53:12 -0300

    Interfaces Task 2

---
+++ Interfaces_Exercicios/resumo_pedido/ProcessadorPedido.java
@@ -0,0 +1,36 @@
+package provedores;
+
+import .Frete;
+import provedores.ProvedorFrete;
+
+public class ProcessadorPedido {
+    private ProvedorFrete provedorFrete ;
+
+
+
+
+
+
+    public ProcessadorPedido(ProvedorFrete provedorFrete) {
+        this.provedorFrete = provedorFrete;
+    }
+
+    public void processar(Pedido pedido){
+
+       Frete resultado =  provedorFrete.calcularFrete(pedido.getPeso(), pedido.getTotal());
+
+       pedido.setFrete(resultado);
+
+
+
+        
+    }
+
+    public ProvedorFrete getProvedorFrete() {
+        return provedorFrete;
+    }
+
+    public void setProvedorFrete(ProvedorFrete provedorFrete) {
+        this.provedorFrete = provedorFrete;
+    }
+}
