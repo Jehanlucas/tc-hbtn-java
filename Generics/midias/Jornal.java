97e1b2ea56911227eab832669ba9c6552499dc2a
Jehanlucas <jehan@ciandt.com>
2022-07-06 11:15:41 -0300

    Generics

---
+++ Generics/midias/Jornal.java
@@ -0,0 +1,21 @@
+
+
+public class Jornal extends Midia{
+
+    private int quantidadeArtigos ;
+
+
+
+    public Jornal(String nome, int quantidadeArtigos) {
+        super.setNome(nome);
+        this.quantidadeArtigos = quantidadeArtigos;
+    }
+
+    public int getQuantidadeArtigos() {
+        return quantidadeArtigos;
+    }
+
+    public void setQuantidadeArtigos(int quantidadeArtigos) {
+        this.quantidadeArtigos = quantidadeArtigos;
+    }
+}
