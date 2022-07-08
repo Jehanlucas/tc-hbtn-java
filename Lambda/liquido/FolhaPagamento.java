8048b1312394312dcfaa44a4157d9aa7cad4c3a6
Jehanlucas <jehan@ciandt.com>
2022-07-08 10:03:25 -0300

    Lambda Exercicio 01

---
+++ Lambda/liquido/FolhaPagamento.java
@@ -0,0 +1,6 @@
+public class FolhaPagamento {
+
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario pagamento){
+        return pagamento.calcular(funcionario.getSalario());
+    }
+}
