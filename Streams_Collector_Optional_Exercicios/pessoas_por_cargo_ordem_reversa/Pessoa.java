bacd5bc1317a95f6bc5fa4fd28bd51dd02f7811a
Jehanlucas <107195319+Jehanlucas@users.noreply.github.com>
2022-07-13 15:40:29 -0300

    Update Pessoa.java

--- Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -57,7 +57,7 @@ public class Pessoa implements Comparable<Pessoa>{
 
    @Override
    public String toString() {
-      return   String.format("[%d] %s %s R$v%d %f ",codigo,nome,cargo,idade,salario);
+      return   String.format("[%d] %s %s %d R$ %f ",codigo,nome,cargo,idade,salario);
 
    }
 
